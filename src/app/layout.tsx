2c77e25e590d51348d6480560de847c04c394508
cramos108 <celioramosnow@gmail.com>
2026-09-09 17:49:03 -0400

    Add the Meta Pixel to the site layout.
    Initialize pixel 1586394896313266 and track PageView on every page, including the noscript fallback.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,14 @@
 import type { Metadata, Viewport } from "next";
 import { headers } from "next/headers";
+import Script from "next/script";
 import { Geist, Geist_Mono } from "next/font/google";
 import { Analytics } from "@vercel/analytics/next";
 import { DomainGate } from "@/components/DomainGate";
 import { APP_NAME, APP_TAGLINE } from "@/lib/brand";
 import { hostnameFromHost } from "@/lib/site";
 import "./globals.css";
+
+const META_PIXEL_ID = "1586394896313266";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -60,6 +63,29 @@ export default async function RootLayout({ children }: LayoutProps<"/">) {
       <body className="min-h-full bg-[#0F172A] text-white">
         <DomainGate serverHostname={hostname}>{children}</DomainGate>
         <Analytics />
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '${META_PIXEL_ID}');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src={`https://www.facebook.com/tr?id=${META_PIXEL_ID}&ev=PageView&noscript=1`}
+            alt=""
+          />
+        </noscript>
       </body>
     </html>
   );
